1677ee075db178b63cf907710a81302fed0affd7
SadieLewis <you@example.com>
2025-12-14 03:02:41 -0600

    Adjusted environment

--- environment.py
+++ environment.py
@@ -1,25 +1,28 @@
 import random
 class TamagotchiEnv:
     def __init__(self):
-        self.state = None
         self.states = ["hungry", "energetic", "sleepy"]
         self.actions = ["eat", "play", "rest"]
         self.correct_pairs = {
-            "hungry": "eat",
-            "energetic": "play",
-            "sleepy": "rest"
+            #hungry & eat
+            0:0,
+            #energetic & play
+            1:1,
+            #sleepy & rest
+            2:2
         }
-        self.reset()
+        self.num_states = len(self.states)
+        self.num_actions = len(self.actions)
+        self.state = None
 
     def reset(self):
-        self.state = random.choice(self.states)
+        self.state = random.randint(0, self.num_states - 1)
         return self.state
 
     def step(self, action):
-        # Takes an action and returns next_state, reward
         if action == self.correct_pairs[self.state]:
             reward = 10
         else:
             reward = -5
-        self.state = random.choice(self.states)
+        self.state = random.randint(0, self.num_states - 1)
         return self.state, reward
