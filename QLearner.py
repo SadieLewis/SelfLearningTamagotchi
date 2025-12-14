397b9dffa8519c9bdf64b9deb44d5cfee99e387f
SadieLewis <you@example.com>
2025-12-14 02:29:51 -0600

    Made QLearner

---
+++ QLearner.py
@@ -0,0 +1,11 @@
+from QTable import QTable
+class QLearner:
+    def __init__(self, num_states, num_actions):
+        self.qtable = QTable(num_states, num_actions, start_state=0)
+
+    def act_and_learn(self, new_state, reward):
+        return self.qtable.sense_act_learn(new_state, reward)
+
+    @property
+    def last_action(self):
+        return self.qtable.last_action
